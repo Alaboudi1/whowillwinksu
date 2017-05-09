6eefba6effafaa0798105d80b6b9952fb6a05301
Abdulaziz <aalabodi@ksu.edu.sa>
2017-05-09 19:39:52 +0000

    first commit

--- db/dbConnection.php
+++ db/dbConnection.php
@@ -2,7 +2,7 @@
 // connection credentials
     $host = "mysql1.gear.host";
     $user = "opensource";
-    $pass = "openSource@123";
+    $pass = "opensource@123";
     $db = "openwhowillwin";
     $port = 3306;
     $connection = mysqli_connect($host, $user, $pass, $db, $port)or die(mysql_error());
